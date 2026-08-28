82ba6a8f51776a62bb3556e6ee86d88aad2eba89
sophiasussa <sophiasussa@outlook.com>
2026-08-28 09:57:04 -0300

    feat: adiciona outputs do RDS

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,1 +1,14 @@
+output "rds_endpoint" {
+  description = "Endpoint do RDS PostgreSQL."
+  value       = aws_db_instance.postgres.address
+}
 
+output "rds_port" {
+  description = "Porta do PostgreSQL."
+  value       = aws_db_instance.postgres.port
+}
+
+output "rds_security_group_id" {
+  description = "ID do Security Group do RDS."
+  value       = aws_security_group.rds.id
+}
