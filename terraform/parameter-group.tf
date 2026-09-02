2f6b35cb25ab47b2d155e883b0f9346c413c7890
sophiasussa <sophiasussa@outlook.com>
2026-09-01 23:52:00 -0300

    feat: adiciona parâmetros para monitoramento e análise de queries

--- terraform/parameter-group.tf
+++ terraform/parameter-group.tf
@@ -8,6 +8,24 @@ resource "aws_db_parameter_group" "postgres" {
     apply_method = "pending-reboot"
   }
 
+  parameter {
+    name         = "log_min_duration_statement"
+    value        = "1000"
+    apply_method = "immediate"
+  }
+
+  parameter {
+    name         = "pg_stat_statements.track"
+    value        = "all"
+    apply_method = "immediate"
+  }
+
+  parameter {
+    name         = "track_io_timing"
+    value        = "1"
+    apply_method = "immediate"
+  }
+
   tags = {
     Name = "oficina-postgres-17"
   }
