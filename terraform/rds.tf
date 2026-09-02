af173de1595c424f7e0bfd1430debfa8f548df96
sophiasussa <sophiasussa@outlook.com>
2026-09-01 23:09:49 -0300

    feat: configura versão, criptografia e tags do PostgreSQL

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -1,7 +1,8 @@
 resource "aws_db_instance" "postgres" {
   identifier = "oficina-postgres"
 
-  engine = "postgres"
+  engine         = "postgres"
+  engine_version = "17.11"
 
   db_name  = var.db_name
   username = var.db_username
@@ -10,6 +11,8 @@ resource "aws_db_instance" "postgres" {
   instance_class    = var.db_instance_class
   allocated_storage = 20
   storage_type      = "gp3"
+
+  storage_encrypted = true
 
   db_subnet_group_name = aws_db_subnet_group.postgres.name
 
@@ -25,4 +28,8 @@ resource "aws_db_instance" "postgres" {
 
   deletion_protection = false
   skip_final_snapshot = true
+
+  tags = {
+    Name = "oficina-postgres"
+  }
 }
