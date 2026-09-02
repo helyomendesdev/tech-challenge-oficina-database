2af61d8790e3354bfe0f4a61efc52ff02a4c178f
sophiasussa <sophiasussa@outlook.com>
2026-09-01 23:02:51 -0300

    feat: configura reinicialização pendente para parâmetros do RDS

--- terraform/parameter-group.tf
+++ terraform/parameter-group.tf
@@ -3,8 +3,9 @@ resource "aws_db_parameter_group" "postgres" {
   family = "postgres17"
 
   parameter {
-    name  = "shared_preload_libraries"
-    value = "pg_stat_statements"
+    name         = "shared_preload_libraries"
+    value        = "pg_stat_statements"
+    apply_method = "pending-reboot"
   }
 
   tags = {
